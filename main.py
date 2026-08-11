e02502397ad7a93b0956409fc4a5bab1b44ad62e
sayeribhattacharjee02-droid <sayeribhattacharjee02@gmail.com>
2026-08-11 12:17:32 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hello! I am AIO.")
+print("🤖 Welcome to AIO!")
+print("Your friendly helper for student jobs, scam alerts, and fun.")
+print("🤖 Welcome to AIO!")
+
+name = input("What is your name? ")
+
+print("Hello, " + name + "!")
+print("I can help with student jobs, scam alerts, and fun.")
